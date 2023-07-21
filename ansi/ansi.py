2fd1eedb7c18ee8eee4de3f5c5a8fa2a4b0c2337
El Codigo Dominicano <Anburivas@gmail.com>
2023-07-21 00:38:48 -0400

    Update ansi.py

--- ansi/ansi.py
+++ ansi/ansi.py
@@ -5,8 +5,9 @@ constants for various uses to spice up your terminal !
 this dataclass can be used as-is without the other two files
 can be imported into your own projects.
 
-NOTE: Some colors might, might not display properly (or none at all even)
-that is dependant on your terminal/command prompt.
+NOTE: Some colors might, might not display properly
+(or none at al) that is dependant on your terminal/command 
+prompt, operating system of choice.
 
 Author: AERivas
 Date: 07/11/2023
